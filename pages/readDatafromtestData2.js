43af05b33ad2095ebd40a730adec58b1f1335396
Praveen <praveengu19@gmail.com>
2026-09-17 22:06:39 +0530

    Constructor Fix

--- pages/readDatafromtestData2.js
+++ pages/readDatafromtestData2.js
@@ -5,7 +5,7 @@ export class DataDriven2{
     constructor(page){
         this.page = page;
         this.searchbox = '[id="twotabsearchtextbox"]'
-        
+        this.searchbutton = '[id="nav-search-submit-button"]'
         
         this.searchresult = '[cel_widget_id="MAIN-SEARCH_RESULTS-4"]'
         this.searchresult2 = '[class="a-color-state a-text-bold"]'
